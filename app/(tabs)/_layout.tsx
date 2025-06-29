b36ca777ab7951df87ef5478c08c9c12fd465858
lee juyoung <juyung0903@naver.com>
2025-06-29 23:05:51 +0900

    feat: tabs 라우팅에 커스텀 탭 리스트 및 버튼 추가

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,9 +1,33 @@
-import { Tabs } from 'expo-router';
+import CustomTabButton from '@/conponents/CustomTabBar/CustomTabButton';
+import { CustomTabList } from '@/conponents/CustomTabBar/CustomTabList';
+import FloatingButton from '@/conponents/CustomTabBar/FloatingButton';
+import { TabList, Tabs, TabSlot, TabTrigger } from 'expo-router/ui';
 
 export default function TabLayout() {
   return (
     <Tabs>
-      <Tabs.Screen name="index" options={{ title: 'Home' }} />
+      <TabSlot />
+      <TabList asChild>
+        <CustomTabList>
+          <TabTrigger name="home" href="/" asChild>
+            <CustomTabButton routeName='home'/>
+          </TabTrigger>
+          <TabTrigger name="explore" href="/explore" asChild>
+            <CustomTabButton routeName='explore' />
+          </TabTrigger>
+           <TabTrigger name="schedule" href="/schedule" asChild>
+            <FloatingButton />
+          </TabTrigger>
+          <TabTrigger name="community" href="/community" asChild>
+            <CustomTabButton routeName='community' />
+          </TabTrigger>
+          <TabTrigger name="user" href="/user" asChild>
+            <CustomTabButton routeName='user' />
+          </TabTrigger>
+        </CustomTabList>
+
+      </TabList>
     </Tabs>
+    // </SafeAreaView>
   );
 }
