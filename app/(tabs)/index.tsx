04234594d4f2ffe1f69466d421c17f874b9190a0
devhaeun <lala819010@gmail.com>
2025-06-29 23:17:36 +0900

    feat: 홈 화면 개발 중 - SafeArea 및 Scroll 적용 - 필요한 텍스트 임의 배치

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,48 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, ScrollView, StyleSheet, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Text>홈페이지</Text>
+      <SafeAreaView>
+        <ScrollView contentContainerStyle={styles.scrollContainer}>
+          <View style={styles.myStatus}>
+            <View style={styles.flightLevel}>
+              <Text>LOGO</Text>
+              <Text>설악산 글라이더</Text>
+              <Text>김유이 님</Text>
+            </View>
+            <View style={styles.travelCard}>
+              <View>
+                <Text>양평 여행 (3일)</Text>
+                <Text> 07.24 - 07. 26</Text>
+              </View>
+              <View>
+                <View>
+                  <Text>1일차</Text>
+                  <Text>양평 패러러브 패러글라이딩</Text>
+                </View>
+                <View>
+                  <Text>2일차</Text>
+                  <Text>000 드라마 세트장</Text>
+                </View>
+                <View>
+                  <Text>3일차</Text>
+                  <Text>양평 해수욕장</Text>
+                </View>
+              </View>
+              <View style={styles.cardBottom}>
+                <Text>2인</Text>
+                <Button onPress={() => {}} title="일정보기" />
+              </View>
+            </View>
+            <Text>홈페이지</Text>
+            <Text>홈페이지</Text>
+            <Text>홈페이지</Text>
+            <Text>홈페이지</Text>
+          </View>
+        </ScrollView>
+      </SafeAreaView>
     </View>
   );
 }
@@ -11,6 +50,25 @@ export default function Index() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
+    alignItems: "center",
+  },
+  scrollContainer: {
+    width: 400,
+    backgroundColor: "#EAF2FC",
+    alignItems: "center",
+  },
+  myStatus: {},
+  flightLevel: {},
+  travelCard: {
+    width: 354,
+    height: 210,
+    flexShrink: 0,
+    borderRadius: 12,
+    backgroundColor: "rgba(255, 255, 255, 0.6)",
+  },
+  cardBottom: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
   },
 });
