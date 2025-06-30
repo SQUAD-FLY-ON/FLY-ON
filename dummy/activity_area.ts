3156af925eec354cb4c616707b0315df7ccefbd4
lee juyoung <juyung0903@naver.com>
2025-06-30 23:36:22 +0900

    feat: 메인 화면 가까운 체험장 추천

---
+++ dummy/activity_area.ts
@@ -0,0 +1,39 @@
+import { activityArea } from "@/types";
+
+export const activities :activityArea[] = [
+  {
+    id: 1,
+    image: require('@/assets/images/dummy_image_activity_area.png'),
+    title: '[양평] 양평 패러러브 패러글라이딩',
+    score: 4.9,
+    reviews: 19,
+  },
+  {
+    id: 2,
+    image: require('@/assets/images/dummy_image_activity_area.png'),
+    title: '[단양] 스카이패러 단양 패러글라이딩',
+    score: 4.8,
+    reviews: 35,
+  },
+  {
+    id: 3,
+    image: require('@/assets/images/dummy_image_activity_area.png'),
+    title: '[가평] 플라이가평 패러글라이딩 체험',
+    score: 4.7,
+    reviews: 22,
+  },
+  {
+    id: 4,
+    image: require('@/assets/images/dummy_image_activity_area.png'),
+    title: '[제주] 제주 드림윙 패러글라이딩',
+    score: 4.6,
+    reviews: 41,
+  },
+  {
+    id: 5,
+    image: require('@/assets/images/dummy_image_activity_area.png'),
+    title: '[포항] 해맞이 패러글라이딩 체험장',
+    score: 4.9,
+    reviews: 28,
+  },
+];
