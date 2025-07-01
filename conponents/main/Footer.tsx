6eb0eaafaa6372b78e8a9da5451b1b7f32186df3
lee juyoung <juyung0903@naver.com>
2025-07-01 16:44:05 +0900

    feat: 메인화면 푸터

---
+++ conponents/main/Footer.tsx
@@ -0,0 +1,40 @@
+import { Image, StyleSheet, Text, View } from "react-native";
+
+export default function Footer() {
+    return (<View style={styles.container}>
+        <View style={styles.footerContainer}>
+            <View style={styles.footerRow}>
+                <Text style={styles.text}>이용약관</Text>
+                <Text style={styles.text}>고객센터</Text>
+                <Text style={styles.text}>운영정책</Text>
+                <Text style={styles.text}>신고센터</Text>
+            </View>
+            <Image source={require('@/assets/images/logo.png')} />
+        </View>
+    </View>)
+}
+const styles = StyleSheet.create({
+    container: {
+        width: '100%',
+        backgroundColor: '#ECF4FE',
+    },
+    footerContainer: {
+        gap: 8,
+        marginTop: 41,
+        alignItems: 'center',
+        marginBottom: 50,
+    },
+    footerRow: {
+        flexDirection: 'row',
+        gap: 20,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    text: {
+        color: '#747474',
+        fontWeight: 400,
+        fontSize: 14,
+        height: 17,
+        lineHeight: 17,
+    }
+})
