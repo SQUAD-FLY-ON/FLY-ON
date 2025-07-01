d24e4ec4f11a73fd47e0ba5e62e47f74350d2e1f
devhaeun <lala819010@gmail.com>
2025-07-01 15:08:44 +0900

    feat: 전역 색상 및 그라데이션 컴포넌트 - 전역 색상표 consants 폴더에 추가 - 전역으로 사용 가능한 MainGradient 컴포넌트 생성

---
+++ conponents/LinearGradients/MainGradient.tsx
@@ -0,0 +1,23 @@
+import Colors from "@/constants/colors";
+import { LinearGradient } from "expo-linear-gradient";
+import React from "react";
+import { StyleProp, ViewStyle } from "react-native";
+
+interface Props {
+  children?: React.ReactNode;
+  style?: StyleProp<ViewStyle>;
+}
+
+export const MainGradient: React.FC<Props> = ({ children, style }) => {
+  return (
+    <LinearGradient
+      colors={Colors.mainGradation.colors}
+      locations={Colors.mainGradation.locations}
+      start={{ x: 0.1, y: 0 }}
+      end={{ x: 0.9, y: 1 }}
+      style={style}
+    >
+      {children}
+    </LinearGradient>
+  );
+};
