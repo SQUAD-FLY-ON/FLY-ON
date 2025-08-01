2e1470b5219c4cc7b419e443211c4c0fa7f2e719
lee juyoung <juyung0903@naver.com>
2025-08-01 18:49:24 +0900

    fix:  패러글라이딩 지역을 제외한 나머지 지역 삭제

--- types/index.tsx
+++ types/index.tsx
@@ -10,7 +10,6 @@ export type activityArea = {
   reviews: number;
 };
 
-export type selectedRegion = { key:string; name: string; coordinates: LatLng[] }
 export type GeoJSONCoordinates = number[][][] | number[][][][];
 
 export type Region = {
@@ -23,3 +22,39 @@ export type Region = {
     CTP_KOR_NM: string;
   };
 };
+
+export type RegionCode =
+  | "11"
+  | "26"
+  | "27"
+  | "29"
+  | "30"
+  | "31"
+  | "41"
+  | "42"
+  | "43"
+  | "44"
+  | "45"
+  | "46"
+  | "47"
+  | "48"
+  | "50";
+
+  export type RegionName =
+  | "서울특별시"
+  | "부산광역시"
+  | "대구광역시"
+  | "광주광역시"
+  | "대전광역시"
+  | "울산광역시"
+  | "경기도"
+  | "강원도"
+  | "충청북도"
+  | "충청남도"
+  | "전라북도"
+  | "전라남도"
+  | "경상북도"
+  | "경상남도"
+  | "제주특별자치도";
+
+  export type selectedRegion = { key:"" | RegionCode; name: string; coordinates: LatLng[] }
