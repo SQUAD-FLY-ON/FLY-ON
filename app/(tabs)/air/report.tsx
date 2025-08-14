f5518eb35544a2f8c68d917382320ce192f5a5b5
devhaeun <lala819010@gmail.com>
2025-08-14 22:19:30 +0900

    feat: 비행기록 리포트 UI

--- app/(tabs)/air/report.tsx
+++ app/(tabs)/air/report.tsx
@@ -1,10 +1,17 @@
 import Background from "@/conponents/(tabs)/air/Background";
+import ReportText from "@/conponents/(tabs)/air/ReportText";
+import { MainGradient } from "@/conponents/LinearGradients/MainGradient";
 import { useSearchParams } from "expo-router/build/hooks";
-import { StyleSheet, Text, View } from "react-native";
+import { Pressable, StyleSheet, Text, View } from "react-native";
 
 export default function Report() {
   const params = useSearchParams() as { time?: string };
   const time = params?.time ? Number(params.time) : 0;
+
+  const minutes = Math.floor(time / 60);
+  const seconds = time - minutes * 60;
+  const minutesStr = minutes < 10 ? "0" + minutes : String(minutes);
+  const secondsStr = seconds < 10 ? "0" + seconds : String(seconds);
 
   return (
     <Background>
@@ -12,10 +19,32 @@ export default function Report() {
       <Text style={styles.description}>
         당신의 비행 데이터를 요약한 리포트를 지금 확인해보세요.
       </Text>
+
       <View style={styles.reportContainer}>
         <View style={styles.reportTitleBox}>
           <Text style={styles.reportTitle}>REPORT</Text>
         </View>
+
+        <View style={styles.reportTextContainer}>
+          <ReportText
+            data={{ label: "비행장", value: "양평 패러리브 패러글라이딩" }}
+          />
+          <ReportText
+            data={{
+              label: "비행 시간",
+              value: `${minutesStr}분 ${secondsStr}초`,
+            }}
+          />
+          <ReportText data={{ label: "비행 고도", value: "000m" }} />
+          <ReportText data={{ label: "비행 거리", value: "000m" }} />
+          <ReportText data={{ label: "평균 비행 속도", value: "00m/s" }} />
+        </View>
+
+        <Pressable>
+          <MainGradient style={styles.flightSaveButton}>
+            <Text style={styles.flightSaveButtonText}>비행기록 저장하기</Text>
+          </MainGradient>
+        </Pressable>
       </View>
     </Background>
   );
@@ -35,11 +64,11 @@ const styles = StyleSheet.create({
     fontSize: 14,
     width: 232,
     textAlign: "center",
-    marginBottom: 40,
+    marginBottom: 20,
   },
   reportContainer: {
     width: 338,
-    height: 443,
+    height: 413,
     borderRadius: 24,
     paddingHorizontal: 10,
     paddingVertical: 16,
@@ -59,5 +88,24 @@ const styles = StyleSheet.create({
     color: "#3A88F4",
     fontFamily: "Pretendard-Bold",
     fontSize: 24,
+  },
+  reportTextContainer: {
+    gap: 20,
+    paddingTop: 18,
+  },
+  flightSaveButton: {
+    marginTop: 48,
+    paddingVertical: 10,
+    paddingHorizontal: 12,
+    width: 306,
+    height: 56,
+    borderRadius: 12,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  flightSaveButtonText: {
+    color: "#fff",
+    fontFamily: "Pretendard-Medium",
+    fontSize: 18,
   },
 });
