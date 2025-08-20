0dcc734d157fb9ed2fd372af770831ffe7fc839f
lee juyoung <juyung0903@naver.com>
2025-08-20 15:00:38 +0900

    feat: 뒤로가기 버튼 press시 뒤로가기 기능

--- conponents/BackButton.tsx
+++ conponents/BackButton.tsx
@@ -1,8 +1,11 @@
 import Entypo from "@expo/vector-icons/Entypo";
-import { View } from "react-native";
+import { useRouter } from "expo-router";
+import { Pressable } from "react-native";
 
 export function BackButton() {
-    return (<View style={{ width: 40, height: 40, justifyContent: 'center', alignItems: 'center' }}>
-        <Entypo name="chevron-left" size={26.67} color="black" />
-    </View>)
+    const router = useRouter();
+    return (
+        <Pressable onPress={() => { router.back() }} style={{ width: 40, height: 40, justifyContent: 'center', alignItems: 'center' }}>
+            <Entypo name="chevron-left" size={26.67} color="black" />
+        </Pressable>)
 }
