f8c37e1c9c6df4bdcec0d0179982c305a8c7f754
lee juyoung <juyung0903@naver.com>
2025-08-27 18:59:07 +0900

    feat: api요청을 위한 세팅

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,16 @@
 import { BottomSheetModalProvider } from "@gorhom/bottom-sheet";
+import {
+  QueryClient,
+  QueryClientProvider
+} from '@tanstack/react-query';
+import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
-import { useFonts } from "expo-font";
 
 export default function RootLayout() {
+  const queryClient = new QueryClient()
+
   const [fontsLoaded] = useFonts({
     "Pretendard-Bold": require("@/assets/fonts/Pretendard-Bold.ttf"),
     "Pretendard-Regular": require("@/assets/fonts/Pretendard-Regular.ttf"),
@@ -12,16 +18,18 @@ export default function RootLayout() {
     "Pretendard-Medium": require("@/assets/fonts/Pretendard-Medium.ttf"),
   });
   return (
-    <SafeAreaProvider>
-      <SafeAreaView style={{ flex: 1 }}>
-        <GestureHandlerRootView style={{ flex: 1 }}>
-          <BottomSheetModalProvider>
-            <Stack>
-              <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-            </Stack>
-          </BottomSheetModalProvider>
-        </GestureHandlerRootView>
-      </SafeAreaView>
-    </SafeAreaProvider>
+    <QueryClientProvider client={queryClient}>
+      <SafeAreaProvider>
+        <SafeAreaView style={{ flex: 1 }}>
+          <GestureHandlerRootView style={{ flex: 1 }}>
+            <BottomSheetModalProvider>
+              <Stack>
+                <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+              </Stack>
+            </BottomSheetModalProvider>
+          </GestureHandlerRootView>
+        </SafeAreaView>
+      </SafeAreaProvider>
+    </QueryClientProvider>
   );
 }
