53062e7d1dafa53a8ed52a05122b2ffd230e1814
lee juyoung <juyung0903@naver.com>
2025-08-28 21:23:50 +0900

    fix: 회원가입 fetch함수의 잘못된 에러메시지 및 주석 수정

--- libs/(tabs)/signup/fetchSignup.ts
+++ libs/(tabs)/signup/fetchSignup.ts
@@ -1,16 +1,18 @@
 import { apiClient } from "@/api/apiClient";
 import { ApiResponse, SignupRequest } from "@/types/api";
+import { Alert } from "react-native";
 
 /**
- * 패러글라이딩 스팟 목록 조회 API
- * @returns 성공 시 Place[] 반환, 실패 시 빈 배열 반환
+ * 회원가입 API
+ * @returns 성공 시 response 반환, 실패 시 빈 배열 반환 및 알림
  */
 export async function fetchSignup(request: SignupRequest): Promise<any> {
   try {
     const response: ApiResponse<any> = await apiClient.post("/members", request);
     return response;
   } catch (error) {
-    console.log("❌ 패러글라이딩 스팟 조회 실패:", error);
+    console.log("❌ 회원가입 실패:", error);
+    Alert.alert('회원가입에 실패하였습니다. 다시시도해주세요.')
     return []; // 실패 시 기본값 반환
   }
 }
