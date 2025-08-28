e6008c1ecb9b9b646696fb2bfd6e601d8e870d62
lee juyoung <juyung0903@naver.com>
2025-08-28 14:36:29 +0900

    refactor: 일정 화면 렌더링 방식 변경으로 성능 최적화

--- conponents/(tabs)/schedule/screens/index.tsx
+++ conponents/(tabs)/schedule/screens/index.tsx
@@ -9,22 +9,21 @@ import SelectPlaceScreen from "./SelectPlaceScreen";
 import SelectRegionScreen from "./SelectRegionScreen";
 type ScreenComponent = React.JSX.Element;
 
-const ScreenMap: Record<ScreenKey, ScreenComponent> = {
-  'SelectDate': <CalanderScreen />,
-  'SelectAreaRegion': <SelectRegionScreen />,
-  'SelectSubRegion': <SelectRegionScreen />,
-  'SelectPlace1': <SelectPlaceScreen />,
-  'SelectPlace2': <SelectPlaceScreen />,
-  'LoadingGenerateSchedule': <GenerateScheduleScreen />,
-  'AIRecommendPlan': <AIRecommendScreen />,
-  'EditPlan': <EditPlanScreen />,
+const ScreenMap: Record<ScreenKey, React.ComponentType<any>> = {
+  'SelectDate': CalanderScreen ,
+  'SelectAreaRegion': SelectRegionScreen,
+  'SelectSubRegion': SelectRegionScreen ,
+  'SelectPlace1': SelectPlaceScreen ,
+  'SelectPlace2': SelectPlaceScreen ,
+  'LoadingGenerateSchedule': GenerateScheduleScreen ,
+  'AIRecommendPlan': AIRecommendScreen ,
+  'EditPlan': EditPlanScreen ,
 };
 export default function Screen() {
   const currentStep = useScheduleStore(state => state.currentStep);
   const currentKey = Screens[currentStep].key as keyof typeof ScreenMap;
-  console.log(currentKey);
-  const ComponentToRender: React.JSX.Element = ScreenMap[currentKey];
+  const ComponentToRender = ScreenMap[currentKey];
   return (<>
-    {ComponentToRender}
+    <ComponentToRender/>
   </>)
 }
