025418a0d5b21ca4bb9117fd4dab24eb2637e1e5
lee juyoung <juyung0903@naver.com>
2025-09-08 20:42:31 +0900

    feat: 회원탈퇴 시 알림창에서 한번더 사용자의 의사 질문

--- conponents/(tabs)/user/MenuList.tsx
+++ conponents/(tabs)/user/MenuList.tsx
@@ -1,6 +1,6 @@
 import { fetchSignout } from "@/libs/(tabs)/user/fetchSignout";
 import { useAuthStore } from "@/store/useAuthStore";
-import { router, useRouter } from "expo-router";
+import { router } from "expo-router";
 import {
   Alert,
   Linking,
@@ -39,14 +39,35 @@ const MenuList = ({ menuItem }: { menuItem: TMenuItem[] }) => {
         router.replace("/intro");
       }
     } else if (name === "회원탈퇴") {
-      const response = await fetchSignout();
-      console.log(response);
-      if (response?.httpStatusCode === 200) {
-        console.log("회원탈퇴 성공");
-        Alert.alert("회원탈퇴가 완료되었습니다");
-        clearAuthState();
-        router.replace("/intro");
-      }
+      Alert.alert(
+    "회원탈퇴", // Alert 제목
+    "정말로 회원을 탈퇴하시겠습니까?", // Alert 내용
+    [
+      {
+        text: "취소", 
+        onPress: () => console.log("회원탈퇴가 취소되었습니다."),
+        style: "cancel", 
+      },
+      {
+        text: "확인", // '확인' 버튼
+        onPress: async () => {
+          // '확인'을 눌렀을 때만 기존 탈퇴 로직을 실행합니다.
+          const response = await fetchSignout();
+          if (response?.httpStatusCode === 200) {
+            console.log("회원탈퇴 성공");
+            Alert.alert("회원탈퇴가 완료되었습니다.");
+            clearAuthState();
+            router.replace("/intro");
+          } else {
+            // 탈퇴 실패 시 사용자에게 알림
+            Alert.alert("오류", "회원탈퇴 처리 중 오류가 발생했습니다.");
+          }
+        },
+        style: "destructive", 
+      },
+    ],
+    { cancelable: false }
+  );
     }
   };
 
