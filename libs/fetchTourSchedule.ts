9923448f26c85d352454ea452e8d8894da58f31b
devhaeun <lala819010@gmail.com>
2025-09-08 20:05:58 +0900

    fix: 문법 오류 및 오타 수정

--- libs/fetchTourSchedule.ts
+++ libs/fetchTourSchedule.ts
@@ -4,13 +4,11 @@ import { Alert } from "react-native";
 
 export async function fetchTourSchedule(): Promise<any> {
   try {
-    const response: ApiResponse<any> = await apiClient.get(
-      `/tourism-schedule`
-    );
+    const response: ApiResponse<any> = await apiClient.get(`/tourism-schedule`);
     return response;
   } catch (error) {
     console.log("❌ 회원정보 조회실패:", error);
-    Alert.alert('여행 일정 조회에 실패하였습니다. 다시 시도해주세요.");
+    Alert.alert("여행 일정 조회에 실패하였습니다. 다시 시도해주세요.");
     return [];
   }
 }
