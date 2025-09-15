61a4c1fbe90719fca12f11a776204a34e7e4b042
lee juyoung <juyung0903@naver.com>
2025-09-15 16:40:33 +0900

    feat: 일정 뒤로가기 버튼

--- conponents/(tabs)/schedule/BackButton.tsx
+++
@@ -1,8 +0,0 @@
-import Entypo from "@expo/vector-icons/Entypo";
-import { View } from "react-native";
-
-export function BackButton() {
-    return (<View style={{ width: 40, height: 40, justifyContent: 'center', alignItems: 'center' }}>
-        <Entypo name="chevron-left" size={26.67} color="black" />
-    </View>)
-}
