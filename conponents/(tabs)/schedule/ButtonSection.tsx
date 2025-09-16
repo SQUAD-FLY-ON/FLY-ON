b2b9cbede05c04f6974a7ea3ecfbd998c91716f1
devhaeun <lala819010@gmail.com>
2025-09-16 20:04:50 +0900

    fix: 여행 일정 추가 코드 useMutation 방식으로 수정

--- conponents/(tabs)/schedule/ButtonSection.tsx
+++ conponents/(tabs)/schedule/ButtonSection.tsx
@@ -4,37 +4,56 @@ import { fetchAddSchedule } from "@/libs/schedule/fetchAddSchedule";
 import { transformDayDataToSchedules } from "@/libs/schedule/transformDayDataToSchedules";
 import { validateNextStepEnabled } from "@/libs/schedule/validateNextStepEnabled";
 import { useScheduleStore } from "@/store/useScheduleStore";
+import { AddScheduleRequest } from "@/types/api";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { useRouter } from "expo-router";
 import React from "react";
 import { Alert, StyleSheet, View } from "react-native";
 
 export default function ButtonSection() {
+  const queryClient = useQueryClient();
+  const mutation = useMutation({
+    mutationFn: async (apiData: AddScheduleRequest) => {
+      return fetchAddSchedule(apiData);
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["mySchedule"] });
+    },
+    onError: (error) => {
+      console.error("여행 일정 추가 실패: ", error);
+    },
+  });
+
   const currentStep = useScheduleStore((state) => state.currentStep);
   const nextEnabled = useScheduleStore(validateNextStepEnabled);
   const goToPrevStep = useScheduleStore((state) => state.goToPrevStep);
   const goToNextStep = useScheduleStore((state) => state.goToNextStep);
-  const currentMarkedDates = useScheduleStore((state) => state.currentMarkedDates);
-  const setCurrentMarkedDates = useScheduleStore((state) => state.setCurrentMarkedDates);
+  const currentMarkedDates = useScheduleStore(
+    (state) => state.currentMarkedDates
+  );
+  const setCurrentMarkedDates = useScheduleStore(
+    (state) => state.setCurrentMarkedDates
+  );
   const resetAllStates = useScheduleStore((state) => state.resetAllStates);
-  const dayData = useScheduleStore(state => state.dayData);
+  const dayData = useScheduleStore((state) => state.dayData);
   const currentStepKey = Screens[currentStep].key;
-  const isAddingSchedule = currentStepKey === 'EditPlan';
-  const isComplete = currentStepKey === 'Complete';
-  const isLoading = currentStepKey.includes('Loading');
-  const prevButtonBackgroundColor = "#D2D2D2"
+  const isAddingSchedule = currentStepKey === "EditPlan";
+  const isComplete = currentStepKey === "Complete";
+  const isLoading = currentStepKey.includes("Loading");
+  const prevButtonBackgroundColor = "#D2D2D2";
   const router = useRouter();
   if (isLoading) {
     return null;
   }
   const resetDates = () => {
     setCurrentMarkedDates({});
-  }
+  };
 
   const handleNextButtonPress = () => {
     if (nextEnabled) {
       goToNextStep();
     }
-  }
+  };
 
   const addSchedule = () => {
     Alert.alert(
@@ -43,7 +62,7 @@ export default function ButtonSection() {
       [
         {
           text: "아니오",
-          onPress: () => { },
+          onPress: () => {},
           style: "cancel",
         },
         {
@@ -56,27 +75,26 @@ export default function ButtonSection() {
               const apiData = {
                 schedules: newSchedule,
                 scheduleStart: dates[0],
-                scheduleEnd: dates[dates.length - 1]
+                scheduleEnd: dates[dates.length - 1],
               };
-
 
               // 2. API 요청을 시도합니다.
               // 이 함수가 성공적으로 완료되면 (에러를 던지지 않으면) 다음 줄로 넘어갑니다.
               // 만약 서버에서 4xx, 5xx 에러를 반환하면 에러가 발생하여 catch 블록으로 이동합니다.
-              const response = await fetchAddSchedule(apiData);
+              // const response = await fetchAddSchedule(apiData);
+              const response = await mutation.mutateAsync(apiData);
+
               // 3. 요청이 성공했을 때만 이 코드가 실행됩니다.
               console.log("일정 추가에 성공했습니다!");
-              if(response.status === 201){
+              if (response.status === 201) {
                 goToNextStep();
-              }
-              else{
+              } else {
                 Alert.alert(
-                "오류",
-                "일정 저장에 실패했습니다. 잠시 후 다시 시도해주세요.",
-                [{ text: "확인" }]
-              );
+                  "오류",
+                  "일정 저장에 실패했습니다. 잠시 후 다시 시도해주세요.",
+                  [{ text: "확인" }]
+                );
               }
-
             } catch (error) {
               // 4. try 블록 안에서 에러가 발생하면 이 코드가 실행됩니다.
               console.error("일정 추가 중 오류 발생:", error);
@@ -93,78 +111,90 @@ export default function ButtonSection() {
       ],
       { cancelable: false }
     );
-  }
-  const LeftButton = currentStep > 0 ? (
-    <CustomButton
-      onPress={goToPrevStep}
-      text='이전 단계'
-      style={styles.prevButton}
-      textStyle={styles.prevText}
-      backgroundColor={prevButtonBackgroundColor} />
-  ) : (
-    <CustomButton
-      undo
-      onPress={resetDates}
-      text='초기화'
-      style={styles.prevButton}
-      textStyle={styles.prevText}
-      backgroundColor={prevButtonBackgroundColor} />
-  );
+  };
+  const LeftButton =
+    currentStep > 0 ? (
+      <CustomButton
+        onPress={goToPrevStep}
+        text="이전 단계"
+        style={styles.prevButton}
+        textStyle={styles.prevText}
+        backgroundColor={prevButtonBackgroundColor}
+      />
+    ) : (
+      <CustomButton
+        undo
+        onPress={resetDates}
+        text="초기화"
+        style={styles.prevButton}
+        textStyle={styles.prevText}
+        backgroundColor={prevButtonBackgroundColor}
+      />
+    );
 
-  const RightButton =
+  const RightButton = (
     <CustomButton
       text={isAddingSchedule ? "일정 추가하기" : "다음 단계"}
       onPress={isAddingSchedule ? addSchedule : handleNextButtonPress}
       containerStyle={{ flex: 1 }}
       textStyle={styles.rightButtonText}
-      disabled={!nextEnabled} />
+      disabled={!nextEnabled}
+    />
+  );
 
-  return (<View style={[styles.container, currentStepKey.includes('Loading') && { display: 'none' }]}>
-    {
-      isComplete? (
-        <CustomButton text="홈으로" onPress={() => {
-          resetAllStates();
-          router.push('/')
-          
-        }}  containerStyle={{ flex: 1, maxWidth: 354 }}/>
-      ):
-      (<>
-    {LeftButton}
-    {RightButton}
-      </>)
-    }
-  </View>)
+  return (
+    <View
+      style={[
+        styles.container,
+        currentStepKey.includes("Loading") && { display: "none" },
+      ]}
+    >
+      {isComplete ? (
+        <CustomButton
+          text="홈으로"
+          onPress={() => {
+            resetAllStates();
+            router.push("/");
+          }}
+          containerStyle={{ flex: 1, maxWidth: 354 }}
+        />
+      ) : (
+        <>
+          {LeftButton}
+          {RightButton}
+        </>
+      )}
+    </View>
+  );
 }
-
 
 const styles = StyleSheet.create({
   container: {
-    flexDirection: 'row',
+    flexDirection: "row",
     gap: 12,
-    width: '100%',
+    width: "100%",
   },
   prevButton: {
     paddingHorizontal: 17.5,
-    flexDirection: 'row',
+    flexDirection: "row",
     gap: 6,
-    alignItems: 'center'
+    alignItems: "center",
   },
   nextButton: {
     paddingHorizontal: 17.5,
-    alignItems: 'center'
+    alignItems: "center",
   },
   prevText: {
-    color: '#747474',
+    color: "#747474",
     fontSize: 20,
     lineHeight: 20,
     fontWeight: 500,
-    fontFamily: 'Pretendard-Medium'
+    fontFamily: "Pretendard-Medium",
   },
   rightButtonText: {
-    fontFamily: 'Pretendard-Medium',
+    fontFamily: "Pretendard-Medium",
     fontSize: 20,
     fontWeight: 500,
-    lineHeight: 20
-  }
-
-})
+    lineHeight: 20,
+  },
+});
