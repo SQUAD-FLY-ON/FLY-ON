9e54e49ea18a7bbb4f6b50e1ba94087420877328
lee juyoung <juyung0903@naver.com>
2025-09-20 22:37:37 +0900

    feat: 일정 라우팅

--- app/(tabs)/my-schedules/index.tsx
+++ app/(tabs)/my-schedules/index.tsx
@@ -1,27 +1,21 @@
 import TravelCard from "@/conponents/(tabs)/my-schedule/TravelCard/TravelCard";
 import Header from "@/conponents/Header";
 import { useTourSchedule } from "@/hooks/useTourSchedule";
+import { useRouter } from "expo-router";
 import { FlatList, StyleSheet, View } from "react-native";
 
 export default function Index() {
-  const { schedule: response } = useTourSchedule();
-  const schedules = response?.filter(
-    (schedule) => schedule.dailyTourismSpots !== null
-  );
-  console.log(schedules);
+  const { isScheduleLoading, isScheduleError, schedule } = useTourSchedule();
+  const router = useRouter();
+  console.log(schedule);
   return (
     <View style={styles.container}>
       <Header title="나의 여행" backButton={false} />
       <FlatList
-        data={schedules}
-        style={{ flex: 1, width: "100%" }}
-        contentContainerStyle={{
-          paddingHorizontal: 16,
-          gap: 24,
-          paddingBottom: 120,
-        }}
-        renderItem={({ item }) => <TravelCard key={item.id} schedule={item} />}
-      />
+        data={schedule}
+        style={{ flex: 1, width: '100%' }}
+        contentContainerStyle={{ paddingHorizontal: 16, gap: 24, paddingBottom: 120 }}
+        renderItem={({ item }) => <TravelCard onPress= {() => {router.push(`/(tabs)/my-schedules/detail/${item.id}`)}} key={item.id} schedule={item} />} />
     </View>
   );
 }
