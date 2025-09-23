991b84323d920e2e94cad54bc28b7911e3271a05
lee juyoung <juyung0903@naver.com>
2025-09-23 16:10:46 +0900

    fix: 비밀번호 정규표현식 수정

--- schema/signupSchema.ts
+++ schema/signupSchema.ts
@@ -17,16 +17,14 @@ export const signUpSchema = z.object({
     .max(20, { message: '아이디는 최대 20글자까지만 가능합니다.' })
     .regex(/^[a-z0-9]+$/, { message: '아이디는 영소문자, 숫자만 사용할 수 있습니다.' })
   ,
-
   password: z
     .string({
       error: (iss) => iss.input === undefined ? "비밀번호는 필수 입력값입니다." : "Invalid input."
     })
-    // .nonempty('비밀번호는 필수입력항목입니다.')
     .min(8, { message: '비밀번호는 최소 8글자 이상이어야 합니다.' })
-    .regex(/^(?=.*[a-zA-Z])(?=.*[0-9])[a-zA-Z0-9]+$/, {
-      message: '비밀번호는 영문과 숫자를 모두 포함해야 합니다.',
-}),
+    .regex(/^(?=.*[a-zA-Z])(?=.*[0-9]).+$/, {
+      message: '비밀번호는 영문과 숫자를 모두 포함해야 합니다.',
+    }),
   passwordConfirm: z.string({
     error: (iss) => iss.input === undefined ? "비밀번호가 일치하지 않습니다." : "Invalid input."
   }),
