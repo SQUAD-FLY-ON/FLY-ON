4403703837866c6eb702219c0d46e23917fd14d1
lee juyoung <juyung0903@naver.com>
2025-10-18 21:25:48 +0900

    fix: 체험장 다른지역 선택방지 및 선택해야만 장소선택으로 넘어가게

--- conponents/(tabs)/schedule/screens/index.tsx
+++ conponents/(tabs)/schedule/screens/index.tsx
@@ -1,6 +1,8 @@
 import { Screens } from "@/constants/screens";
 import { useScheduleStore } from "@/store/useScheduleStore";
 import { ScreenKey } from "@/types";
+import { useEffect } from "react";
+import { useShallow } from "zustand/shallow";
 import CalanderScreen from "./CalanderScreen";
 import CompleteScreen from "./CompleteScreen";
 import GenerateScheduleScreen from "./GenerateScheduleScreen";
@@ -22,8 +24,11 @@ const ScreenMap: Record<ScreenKey, React.ComponentType<any>> = {
   "Complete": CompleteScreen,
 };
 export default function Screen() {
-  const currentStep = useScheduleStore(state => state.currentStep);
+  const { currentStep ,selectedRegion , refreshSelectedActivities}  = useScheduleStore(useShallow(state => ({currentStep: state.currentStep, selectedRegion: state.selectedRegion, refreshSelectedActivities: state.refreshSelectedActivities})));
   const currentKey = Screens[currentStep].key as keyof typeof ScreenMap;
+  useEffect(() => {
+    refreshSelectedActivities();
+  }, [selectedRegion])
   const ComponentToRender = ScreenMap[currentKey];
   return (<>
     <ComponentToRender/>
