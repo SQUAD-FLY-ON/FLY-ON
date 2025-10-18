58e45a41c00237d02236882e1a204a7c0e3c8cb4
lee juyoung <juyung0903@naver.com>
2025-10-18 21:52:32 +0900

    fix: screens의 index에있던 체험장 초기화 코드 지역선택스크린으로 옮김

--- conponents/(tabs)/schedule/screens/index.tsx
+++ conponents/(tabs)/schedule/screens/index.tsx
@@ -1,7 +1,6 @@
 import { Screens } from "@/constants/screens";
 import { useScheduleStore } from "@/store/useScheduleStore";
 import { ScreenKey } from "@/types";
-import { useEffect } from "react";
 import { useShallow } from "zustand/shallow";
 import CalanderScreen from "./CalanderScreen";
 import CompleteScreen from "./CompleteScreen";
@@ -24,11 +23,9 @@ const ScreenMap: Record<ScreenKey, React.ComponentType<any>> = {
   "Complete": CompleteScreen,
 };
 export default function Screen() {
-  const { currentStep ,selectedRegion , refreshSelectedActivities}  = useScheduleStore(useShallow(state => ({currentStep: state.currentStep, selectedRegion: state.selectedRegion, refreshSelectedActivities: state.refreshSelectedActivities})));
+  const { currentStep }  = useScheduleStore(useShallow(state => ({currentStep: state.currentStep})));
   const currentKey = Screens[currentStep].key as keyof typeof ScreenMap;
-  useEffect(() => {
-    refreshSelectedActivities();
-  }, [selectedRegion])
+
   const ComponentToRender = ScreenMap[currentKey];
   return (<>
     <ComponentToRender/>
