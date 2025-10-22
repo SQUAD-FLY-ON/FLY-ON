7c237ed5e8e0c558b390fc45b7c977df1a36051e
devhaeun <lala819010@gmail.com>
2025-10-23 04:54:19 +0900

    fix: 체험장 상세 페이지 Cesium 영상 오류 해결 중 - fetchSpotTrack: API 불러오기 오류 발생

--- libs/(tabs)/explore/detail/fetchSpotTrack.ts
+++ libs/(tabs)/explore/detail/fetchSpotTrack.ts
@@ -3,22 +3,24 @@ import { ITrackData, ITrackPoints } from "@/types";
 import { ApiResponse } from "@/types/api";
 
 /**
- * 체험장 상세 정보 조회 API
+ * 체험장 비행 경로 제공 API
  * @returns 성공 시 ITrackData[] 반환, 실패 시 null 반환
  */
 export async function fetchSpotTrack(
-  spotId: string
+  spotId: string,
+  memberId: string
 ): Promise<ITrackData[] | null> {
+  console.log("spotId:", spotId);
   try {
     const response: ApiResponse<ITrackPoints> = await apiClient.get(
       `/flight-logs/${spotId}/track`,
       {
-        params: { paraglidingSpotId: spotId },
+        params: { flightLogId: spotId, memberId },
       }
     );
     return response.data.points;
   } catch (error) {
-    console.log("❌ 패러글라이딩 스팟 조회 실패:", error);
+    console.log("❌ 체험장 비행 경로 제공 실패:", error);
     return null;
   }
 }
