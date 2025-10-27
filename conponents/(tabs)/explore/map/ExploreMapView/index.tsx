a7d578c5f8a06b4dbc740e10b0ae4fee0d23cb9a
lee juyoung <juyung0903@naver.com>
2025-10-27 20:46:12 +0900

    fix: 탐색 화면 지도 rn-maps관련 오류

--- conponents/(tabs)/explore/map/ExploreMapView/index.tsx
+++ conponents/(tabs)/explore/map/ExploreMapView/index.tsx
@@ -1,7 +1,7 @@
 import { calculatePolygonCentroid } from '@/libs/map';
 import useExploreStore from '@/store/exploreStore';
-import { MarkerSpotApiRequest } from '@/types/api';
-import { useEffect, useRef, useState } from 'react';
+import { useFocusEffect } from 'expo-router';
+import { useCallback, useRef, useState } from 'react';
 import { StyleSheet } from 'react-native';
 import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
 import { useShallow } from 'zustand/shallow';
@@ -9,7 +9,6 @@ import ExploreSpotMarkers from './ExploreSpotMarkers';
 
 export default function ExploreMap() {
   const mapRef = useRef<MapView | null>(null);
-  const [currentLocation, setCurrentlocation] = useState<MarkerSpotApiRequest | null>(null);
   const { selectedRegion, resetSelectedMarkerSpot } = useExploreStore(useShallow(state => ({ selectedRegion: state.selectedRegion, resetSelectedMarkerSpot: state.resetSelectedMarkerSpot })));
   const latitudes = selectedRegion.coordinates.map(coord => coord.latitude);
   const longitudes = selectedRegion.coordinates.map(coord => coord.longitude);
@@ -17,6 +16,20 @@ export default function ExploreMap() {
   const southWest = { latitude: Math.min(...latitudes), longitude: Math.min(...longitudes) };
   const center = calculatePolygonCentroid(selectedRegion.coordinates);
   console.log(selectedRegion);
+  const [mapReady, setMapReady] = useState(false);
+  useFocusEffect(
+    useCallback(() => {
+      // 화면에 들어올 때
+      setMapReady(true);
+
+      return () => {
+        // 화면을 나갈 때 cleanup
+        setMapReady(false);
+      };
+    }, [])
+  );
+
+
   // 현재 해당하는 지역으로 우선 고정
   // const getBoundaries = async (region: Region) => {
   //   if (mapRef.current) {
@@ -25,11 +38,16 @@ export default function ExploreMap() {
   //     setCurrentlocation({ cornerLatitude, cornerLongitude, centerLatitude, centerLongitude })
   //   }
   // };
-  useEffect(() => {
-    const { latitude: cornerLatitude, longitude: cornerLongitude } = northEast;
-    const { latitude: centerLatitude, longitude: centerLongitude } = center;
-    setCurrentlocation({ cornerLatitude, cornerLongitude, centerLatitude, centerLongitude })
-  }, [selectedRegion])
+  // useEffect(() => {
+  //   const { latitude: cornerLatitude, longitude: cornerLongitude } = northEast;
+  //   const { latitude: centerLatitude, longitude: centerLongitude } = center;
+  //   setCurrentlocation({ cornerLatitude, cornerLongitude, centerLatitude, centerLongitude })
+  // }, [selectedRegion])
+  // useFocusEffect(() => {
+  //   const { latitude: cornerLatitude, longitude: cornerLongitude } = northEast;
+  //   const { latitude: centerLatitude, longitude: centerLongitude } = center;
+  //   setCurrentlocation({ cornerLatitude, cornerLongitude, centerLatitude, centerLongitude })
+  // })
   const restrictBoundaries = () => {
     if (mapRef.current && selectedRegion?.coordinates.length > 0) {
       mapRef.current.setMapBoundaries(northEast, southWest);
@@ -46,28 +64,37 @@ export default function ExploreMap() {
   };
 
   return (
-    <MapView
-      ref={mapRef}
-      provider={PROVIDER_GOOGLE}
-      style={styles.map}
-      userInterfaceStyle={'light'}
-      scrollDuringRotateOrZoomEnabled
-      onMapReady={async () => {
-        updateMapRegion();
-        restrictBoundaries();
-      }}
-      onRegionChangeComplete={async (region) => {
-        // await getBoundaries(region);
-      }}
-      onPress={(event) => { 
-        if (event.nativeEvent.action !== 'marker-press')
-          resetSelectedMarkerSpot();
-      }}
-    >
-      <ExploreSpotMarkers />
-      {/* 지도의 범위와 일치 x */}
-      {/* <Polygon coordinates={selectedRegion.coordinates} /> */}
-    </MapView>
+    <>
+      {
+
+        mapReady &&
+        (
+
+          <MapView
+            ref={mapRef}
+            provider={PROVIDER_GOOGLE}
+            style={styles.map}
+            userInterfaceStyle={'light'}
+            scrollDuringRotateOrZoomEnabled
+            onMapReady={async () => {
+              updateMapRegion();
+              restrictBoundaries();
+            }}
+            onRegionChangeComplete={async (region) => {
+              // await getBoundaries(region);
+            }}
+            onPress={(event) => {
+              if (event.nativeEvent.action !== 'marker-press')
+                resetSelectedMarkerSpot();
+            }}
+          >
+            <ExploreSpotMarkers />
+            {/* 지도의 범위와 일치 x */}
+            {/* <Polygon coordinates={selectedRegion.coordinates} /> */}
+          </MapView>
+        )
+      }
+    </>
   );
 }
 
