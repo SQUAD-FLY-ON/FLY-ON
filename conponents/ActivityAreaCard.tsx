1db2df9b7e3af89bb9b6c5ef8b50eb9b2efe7f6b
lee juyoung <juyung0903@naver.com>
2025-10-27 20:57:32 +0900

    feat:메인화면 체험장 카드에 상세주소 추가 및 press시 체험장 상세페이지로 연결

--- conponents/ActivityAreaCard.tsx
+++ conponents/ActivityAreaCard.tsx
@@ -1,27 +1,43 @@
 // components/MyCard.tsx
+import { polygons } from "@/constants/polygons";
+import { convertCoordinatesToPoints } from "@/libs/regionSelectMap";
+import useExploreStore from "@/store/exploreStore";
 import { RecommendSpots } from "@/types";
+import { useRouter } from "expo-router";
 import React from "react";
-import { Image, StyleSheet, Text, View } from "react-native";
+import { Image, Pressable, StyleSheet, Text } from "react-native";
+import { useShallow } from "zustand/shallow";
 
 export default function ActivityAreaCard({ item }: { item: RecommendSpots }) {
+  const findRegionByName = (name: string) => {
+  return polygons.find((region) => region.properties.CTP_KOR_NM === name);
+};
+
+  const router = useRouter();
+  const { setSelectedRegion, setSelectedMarkerSpot } = useExploreStore(
+  useShallow((state) => ({
+    setSelectedRegion: state.setSelectedRegion,
+    setSelectedMarkerSpot: state.setSelectedMarkerSpot,
+  }))
+);
+  const foundRegion = findRegionByName(item?.sido);
+  const coordinates = convertCoordinatesToPoints(foundRegion?.geometry.coordinates)
   return (
-    <View style={styles.card}>
+    <Pressable style={styles.card} onPress={() => { 
+      router.push(`/explore/detail/${item.id}`) ;
+      setSelectedRegion({key: foundRegion?.properties.CTPRVN_CD || '',name: item?.sido, coordinates});
+      setSelectedMarkerSpot({latitude: item?.latitude, longitude: item?.longitude, id: item?.id, name: item?.spotName});
+    }}>
       <Image source={
-    item?.imgUrl
-      ? { uri: item.imgUrl }: require('@/assets/images/dummy_image_activity_area.png')  } style = {{width:160, height:160, borderRadius:16}} />
+        item?.imgUrl
+          ? { uri: item.imgUrl } : require('@/assets/images/dummy_image_activity_area.png')} style={{ width: 160, height: 160, borderRadius: 16 }} />
       <Text style={styles.text} numberOfLines={2}>
         {item?.spotName}
       </Text>
-      {/* <View style={{ gap: 4, flexDirection: "row" }}>
-        <Image
-          source={require("@/assets/images/star.png")}
-          width={18}
-          height={18}
-        />
-        <Text style={styles.score}>{item.score}</Text>
-        <Text style={styles.review}>({item.reviews})</Text>
-      </View> */}
-    </View>
+      <Text style = {styles.secondText}>
+        {item?.fullAddress}
+      </Text>
+    </Pressable>
   );
 }
 
@@ -42,4 +58,13 @@ const styles = StyleSheet.create({
     fontSize: 14,
     color: "#666",
   },
+  secondText: {
+    fontFamily: "Pretendard-Regular",
+    fontWeight: 300,
+    fontSize: 12,
+    lineHeight: 12 * 1.4, // 140%
+    letterSpacing: 0,
+
+    color: '#747474',
+  }
 });
